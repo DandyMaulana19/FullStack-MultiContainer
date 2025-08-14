6e5e87ca569ef5bdb4d0073a707ad2e81286e10c
DandyMaulana19 <dandyainul19@gmail.com>
2025-08-14 09:58:51 +0700

    feat: customize footer

--- cms-contact-form/resources/views/partials/footer.blade.php
+++ cms-contact-form/resources/views/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="bg-[#2A4D7D] text-white py-4 mt-auto">
     <div class="container mx-auto text-center">
         <p>
-            &copy; 2024 - 
-            <a href="https://triwicaksono.com/?utm_source=labs&utm_medium=demo-projects&utm_campaign=cms-contact-form" 
+            &copy; 2025 - 
+            <a href="https://dandymay.vercel.app/" 
                target="_blank"
                class="text-blue-400 hover:underline">
-               Tri Wicaksono
+               Dandy Maulana
             </a>
         </p>
     </div>
